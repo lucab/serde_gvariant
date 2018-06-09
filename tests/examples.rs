ad8b83a593a702a9aa9d8fa96c94bdc80ee684ab
Luca Bruno <luca.bruno@coreos.com>
2018-06-09 20:33:55 +0000

    lib: add support for Option

--- tests/examples.rs
+++ tests/examples.rs
@@ -12,22 +12,19 @@ fn test_string_example() {
     assert_eq!(de, decoded);
 }
 
-/*
 #[test]
 fn test_maybe_string_example() {
-    let sig_type = "ms";
     let encoded: Vec<u8> = vec![
-        b'h', b'e', b'l', b'l', b'o', b' ', b'w', b'o', b'r', b'l', b'd', b'\0'
+        b'h', b'e', b'l', b'l', b'o', b' ', b'w', b'o', b'r', b'l', b'd', b'\0', b'\0',
     ];
     let decoded: Option<String> = Some("hello world".to_string());
-    let ser: Vec<u8> = serde_gvariant::to_vec(&decoded).expect("String ser");
-    let de: Option<String> = serde_gvariant::from_slice(&encoded[..]).expect("String de");
-    let sig = serde_gvariant::describe(&decoded, None).expect("String describe");
+    let ser: Vec<u8> = serde_gvariant::to_vec(&decoded).expect("Option ser");
+    let de: Option<String> = serde_gvariant::from_slice(&encoded[..]).expect("Option de");
     assert_eq!(ser, encoded);
     assert_eq!(de, decoded);
-    assert_eq!(sig, sig_type);
 }
 
+/*
 #[test]
 fn test_array_booleans_example() {
     let encoded: Vec<u8> = vec![0x01, 0x00, 0x00, 0x01, 0x01];
